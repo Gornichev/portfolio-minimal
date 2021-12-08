22fdc2072eea34efcbde877320b26993450803b4
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2021-12-08 09:35:40 +0200

    swiper

--- int/js/main.js
+++ int/js/main.js
@@ -45,3 +45,45 @@
     window.addEventListener('scroll', checkScroll);
 
 })();
+
+// Swiper
+const swiper = new Swiper('.swiper-cert', {
+    slidesPerView: 5,
+    spaceBetween: 40,
+    speed: 500,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 40
+        },
+
+    }
+});
